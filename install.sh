6dc8b8f132894a1f785118611ad3b6dab8f7188b
小孟要吃肉 <10329613+smallmeng@user.noreply.gitee.com>
2023-02-26 14:04:05 +0000

    update install.sh.
    Signed-off-by: 小孟要吃肉 <10329613+smallmeng@user.noreply.gitee.com>

--- install.sh
+++ install.sh
@@ -10,3 +10,5 @@ bash tar1090-install.sh /run/readsb
 
 rm -rf /usr/local/share/tar1090
 git clone https://gitee.com/smallmeng/tar1090-cn /usr/local/share/tar1090
+
+sed -i -e "s/你的UUID是：/你的UUID是：$(cat /root/get_message/UUID)/g" /usr/local/share/tar1090/html/index.html
